0fe9f436d9a7f0aef3b8d9c0523569a748e2b8b4
prodiptahalder <haldip9500@gmail.com>
2021-01-13 00:05:07 +0530

    Landing Page Added

---
+++ ticketbooking/src/Views/LandingPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../App.css';
+import BG1 from '../assests/images/BG1.svg';
+import logo from '../assests/images/landLOGO.svg';
+import { useHistory } from "react-router-dom";
+
+function LandingPage() {
+  const history = useHistory();
+  return (
+    <div className="container">
+        <div className="landBG">
+            <img src={BG1} style={{width:"100vw"}}/>
+        </div>
+        <div className="landOPAC"/>
+        <div className="landContentBox">
+            <img className="landLOGO" src={logo}/>
+            <button className="landSIGNUP" onClick={() => { history.push("/signin"); }}>Sign Up</button>
+            <button className="landSIGNIN" onClick={() => { history.push("/signup"); }}>Sign In</button>
+        </div>
+    </div>
+  );
+}
+
+export default LandingPage;
